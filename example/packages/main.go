07cea2d747d7e59e109d8746b813843f058a2d49
dengsgo <dengsgo@yoytang.com>
2023-08-19 20:03:54 +0800

    update packages comment

--- example/packages/main.go
+++ example/packages/main.go
@@ -1,13 +1,23 @@
 package main
 
-// Here, use go:decor to import the package where the decorator function is located.
-// Multiple packages can be imported in multiple lines, just like import statements
+import "log"
+
+// The file uses a decorator and needs to be imported anonymously using the package import method.
+// Like this:
+// import _ "github.com/dengsgo/go-decorator/decor"
 //
+// If the decorator used from another package and the package has not been imported,
+// it needs to be imported anonymously.
+// Just like the Fun1 package below:
+// _ "github.com/dengsgo/go-decorator/example/packages/fun1"
+//
+// Why do I need anonymous imports? This is because the go specification does not allow
+// the introduction of packages that are not used.
+// If the package has been imported and used normally, there is no need to import anonymously again
 import (
 	_ "github.com/dengsgo/go-decorator/decor"
 	"github.com/dengsgo/go-decorator/example/packages/fun"
 	_ "github.com/dengsgo/go-decorator/example/packages/fun1"
-	"log"
 )
 
 func main() {
