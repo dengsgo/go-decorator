29124eece0305277b0e2ad81226b8956372607b8
dengsgo <dengsgo@yoytang.com>
2023-10-22 20:29:54 +0800

    update

--- cmd/decorator/checker_test.go
+++ cmd/decorator/checker_test.go
@@ -8,6 +8,6 @@ import (
 func TestCheckDecorAndGetParam(t *testing.T) {
 	param, err := checkDecorAndGetParam("github.com/dengsgo/go-decorator/decor", "find", nil)
 	log.Println(param, err)
-	param, err = checkDecorAndGetParam("github.com/dengsgo/go-decorator/cmd/decorator", "compile", nil)
+	param, err = checkDecorAndGetParam("github.com/dengsgo/go-decorator/cmd/decorator", "logging", nil)
 	log.Println(param, err)
 }
