6ea8a580715af2dbb13736deb2e7943022b2310a
dengsgo <dengsgo@yoytang.com>
2023-11-12 17:30:19 +0800

    update usages

--- example/usages/main.go
+++ example/usages/main.go
@@ -7,13 +7,16 @@ import (
 )
 
 func main() {
+	section("inner.go")
 	// 这是一个使用包内装饰器的函数
 	useScopeInnerDecor("hello, world", 100)
 
+	section("external.go")
 	// 这是一个使用其他包装饰器的函数
 	useExternalaDecor()
 	g.PrintfLn("plus(2, 3) = %+v", plus(2, 3))
 
+	section("datetime.go")
 	// 文档 Guide.md 中演示使用装饰器的代码
 	{
 		t := 1692450000
@@ -21,9 +24,11 @@ func main() {
 		g.Printf("datetime(%d)=%s\n", t, s)
 	}
 
+	section("genericfunc.go")
 	// 泛型函数使用装饰器
 	g.PrintfLn("Sum(1, 2, 3, 4, 5, 6, 7, 8, 9) = %+v", Sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
 
+	section("withdecorparams.go")
 	// 使用带有参数的装饰器，如何传值
 	g.PrintfLn("useArgsDecor() = %+v", useArgsDecor())
 	// 装饰器如何使用 Lint 在编译时约束验证目标函数的参数
@@ -31,6 +36,10 @@ func main() {
 	g.Printf("useHitUseNonzeroLint() = %+v", useHitUseNonzeroLint())
 	g.Printf("useHitBothUseLint() = %+v", useHitBothUseLint())
 	g.Printf("useHitUseMultilineLintDecor() = %+v", useHitUseMultilineLintDecor())
+}
+
+func section(s string) {
+	g.PrintfLn("\n++++++++++ " + s + " ++++++++++")
 }
 
 func init() {
