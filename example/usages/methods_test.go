a9385afaf8b5d4602a3a925ad697cb8ca11e5c00
dengsgo <dengsgo@yoytang.com>
2023-11-18 17:34:00 +0800

    support ctx.TargetName & ctx.Receiver

--- example/usages/methods_test.go
+++ example/usages/methods_test.go
@@ -12,8 +12,8 @@ func TestDoSomething(t *testing.T) {
 		m := &methodTestPointerStruct{}
 		m.doSomething("main called")
 		out := strings.TrimSpace(g.TestBuffers.String())
-		s := `=> dumpDecorContext: Kind: 1, TargetIn: [main called], TargetOut: [], doRef: 0
-<= dumpDecorContext: Kind: 1, TargetIn: [main called], TargetOut: [*methodTestPointerStruct.recPointerDoSomething: main called], doRef: 1`
+		s := `=> dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: &{}, TargetIn: [main called], TargetOut: [], doRef: 0
+<= dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: &{}, TargetIn: [main called], TargetOut: [*methodTestPointerStruct.recPointerDoSomething: main called], doRef: 1`
 		log.Println(out)
 		if out != s {
 			t.Fatalf("TestDoSomething methodTestPointerStruct fail")
@@ -25,8 +25,8 @@ func TestDoSomething(t *testing.T) {
 		m := methodTestRawStruct{}
 		m.doSomething("main called")
 		out := strings.TrimSpace(g.TestBuffers.String())
-		s := `=> dumpDecorContext: Kind: 1, TargetIn: [main called], TargetOut: [], doRef: 0
-<= dumpDecorContext: Kind: 1, TargetIn: [main called], TargetOut: [methodTestRawStruct.recPointerDoSomething: main called], doRef: 1`
+		s := `=> dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: {}, TargetIn: [main called], TargetOut: [], doRef: 0
+<= dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: {}, TargetIn: [main called], TargetOut: [methodTestRawStruct.recPointerDoSomething: main called], doRef: 1`
 		log.Println(out)
 		if out != s {
 			t.Fatalf("TestDoSomething methodTestRawStruct fail")
