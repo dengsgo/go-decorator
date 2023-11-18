bb690d8616326ed6adc82f49f84810ac26276813
dengsgo <dengsgo@yoytang.com>
2023-11-18 17:47:58 +0800

    add test case

--- example/usages/methods_test.go
+++ example/usages/methods_test.go
@@ -7,30 +7,38 @@ import (
 	"testing"
 )
 
-func TestDoSomething(t *testing.T) {
-	t.Run("methodTestPointerStruct", func(t *testing.T) {
-		m := &methodTestPointerStruct{}
-		m.doSomething("main called")
-		out := strings.TrimSpace(g.TestBuffers.String())
-		s := `=> dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: &{}, TargetIn: [main called], TargetOut: [], doRef: 0
+func TestMethodTestPointerStruct_doSomething(t *testing.T) {
+	m := &methodTestPointerStruct{}
+	m.doSomething("main called")
+	out := strings.TrimSpace(g.TestBuffers.String())
+	s := `=> dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: &{}, TargetIn: [main called], TargetOut: [], doRef: 0
 <= dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: &{}, TargetIn: [main called], TargetOut: [*methodTestPointerStruct.recPointerDoSomething: main called], doRef: 1`
-		log.Println(out)
-		if out != s {
-			t.Fatalf("TestDoSomething methodTestPointerStruct fail")
-		}
-		g.ResetTestBuffers()
-	})
+	log.Println(out)
+	if out != s {
+		t.Fatalf("TestDoSomething methodTestPointerStruct fail")
+	}
+	g.ResetTestBuffers()
+}
 
-	t.Run("methodTestRawStruct", func(t *testing.T) {
-		m := methodTestRawStruct{}
-		m.doSomething("main called")
-		out := strings.TrimSpace(g.TestBuffers.String())
-		s := `=> dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: {}, TargetIn: [main called], TargetOut: [], doRef: 0
+func TestMethodTestRawStruct_doSomething(t *testing.T) {
+	m := methodTestRawStruct{}
+	m.doSomething("main called")
+	out := strings.TrimSpace(g.TestBuffers.String())
+	s := `=> dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: {}, TargetIn: [main called], TargetOut: [], doRef: 0
 <= dumpDecorContext: Kind: 1, TargetName: doSomething, Receiver: {}, TargetIn: [main called], TargetOut: [methodTestRawStruct.recPointerDoSomething: main called], doRef: 1`
-		log.Println(out)
-		if out != s {
-			t.Fatalf("TestDoSomething methodTestRawStruct fail")
-		}
-		g.ResetTestBuffers()
-	})
+	log.Println(out)
+	if out != s {
+		t.Fatalf("TestDoSomething methodTestRawStruct fail")
+	}
+	g.ResetTestBuffers()
+}
+
+func TestMethodTestStruct_todo(t *testing.T) {
+	m := &methodTestStruct{}
+	m.todo()
+	s := `validCtxReceiver OK`
+	if s != g.TestBuffers.String() {
+		t.Fatalf("TestMethodTestStruct_todo fail")
+	}
+	g.ResetTestBuffers()
 }
