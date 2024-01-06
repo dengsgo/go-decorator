a2e1c4701cd7234d1a9a3d3b0c983b59c26c4692
dengsgo <dengsgo@yoytang.com>
2024-01-06 20:26:16 +0800

    add wrapped_code comment

--- decor/wrapped_code.go
+++ decor/wrapped_code.go
@@ -1,6 +1,20 @@
 package decor
 
-func wrappedTargetCode( /* in1, in2, ... */) /* (out1, out2, ...) */ {
+// Function `wrappedTargetCode` is only used to show the structure of
+// the target code after being decorated by the decorator.
+// It is not actually executed.
+//
+// If the decorated target code causes a panic at runtime, Go's runtime will
+// print the exception stack. The exception stack usually shows the path and line number
+// of the relevant file where the call was made. This path is not friendly to
+// decorator's real wrapped code.
+// decorator solves this problem by pointing the wrapped function's associated code to
+// this file at compile time.
+//
+// You shouldn't pay too much attention to the presence of this file error in the exception stack,
+// but rather focus on the original target code.
+
+func wrappedTargetCode( /* in1, in2, ... */ ) /* (out1, out2, ...) */ {
 	varDecorContext := Context{
 		Kind:       KFunc, // KFunc / KMethod
 		Receiver:   nil,   // wrapped method receiver
@@ -9,11 +23,11 @@ func wrappedTargetCode( /* in1, in2, ... */) /* (out1, out2, ...) */ {
 		TargetOut:  []any{ /*out1, out2, ....*/ },
 	}
 	varDecorContext.Func = func() {
-		/* varDecorContext.TargetOut[0], varDecorContext.TargetOut[1], ... = */ func( /* in1, in2, ... */) /* (out1, out2, ...) */ {
+		/* varDecorContext.TargetOut[0], varDecorContext.TargetOut[1], ... = */ func( /* in1, in2, ... */ ) /* (out1, out2, ...) */ {
 
 			// Here is code for the original target.
 
-		}( /* varDecorContext.TargetIn[0], varDecorContext.TargetIn[1], ... */)
+		}( /* varDecorContext.TargetIn[0], varDecorContext.TargetIn[1], ... */ )
 	}
 
 	/* decoratorFunc(varDecorContext, ...#{key: value}) */ // execute the decorator function
